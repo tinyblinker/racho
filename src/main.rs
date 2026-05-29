2b1f91199013db66170000706d436638cfefbe37
shyweeds <2149934895@qq.com>
2026-05-29 20:14:06 +0800

    (from asm)Going to the rust_main()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 #![no_std]
 #![no_main]
+
 mod lang_items;
+
+use core::arch::global_asm;
+global_asm!(include_str!("entry.asm"));
+
+#[unsafe(no_mangle)]
+pub fn rust_main() -> ! {
+    clear_bss();
+    loop {}
+}
+fn clear_bss() {
+    unsafe extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    for item in sbss as unsafe extern "C" fn() as usize..ebss as unsafe extern "C" fn() as usize {
+        unsafe {
+            (item as *mut u8).write_volatile(0);
+        }
+    }
+}
