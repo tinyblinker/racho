2b39de76fee5482581ab3d5bd85d50bad36615a8
shyweeds <2149934895@qq.com>
2026-06-02 01:02:04 +0800

    feature:more userlib

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -1,7 +1,27 @@
 #![no_std]
 #![feature(linkage)]
 
+use core::panicking::panic;
+use syscall::*;
+
+pub fn write(fd: usize, buf: &[u8]) -> isize{sys_write(fd, buf)}
+pub fn exit(exit_code: i32)->isize{sys_exit(exit_code)}
+
 #[macro_use]
 pub mod console;
 mod lang_items;
 mod syscall;
+
+#[unsafe(no_mangle)]
+#[link_section = ".text.entry"]
+pub extern "C" fn _start() -> ! {
+    clear_bss();
+    exit(main());
+    panic!("unreachable after sys_exit!");
+}
+
+#[linkage = "weak"]
+#[no_mangle]
+fn main -> i32{
+    panic!("Cannot find main");
+}
