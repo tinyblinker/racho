6d517399d7b4b5c43917741760322e9b5dc50416
shyweeds <2149934895@qq.com>
2026-06-07 18:55:22 +0800

    working on rust_main() in os/src,recommend to analyze the whole boot pipeline when i have some free time

--- os/src/main.rs
+++ os/src/main.rs
@@ -16,12 +16,27 @@ use core::arch::global_asm;
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
 
+/// the rust entry-point of OS
 #[unsafe(no_mangle)]
 pub fn rust_main() -> ! {
+    unsafe extern "C" {
+        safe fn stext(); // begin addr of text segment
+        safe fn etext(); // end addr of text segment
+        safe fn srodata(); // start addr of Read-Only data segment
+        safe fn erodata(); // end addr of Read-Only data ssegment
+        safe fn sdata(); // start addr of data segment
+        safe fn edata(); // end addr of data segment
+        safe fn sbss(); // start addr of BSS segment
+        safe fn ebss(); // end addr of BSS segment
+        safe fn boot_stack_lower_bound(); // stack lower bound
+        safe fn boot_stack_top(); // stack top
+    }
     clear_bss();
-    println!("Hello, world!");
-    panic!("Shutdown machine!");
+    logging::init();
+    println!("[kernel] Hello, world!");
+    loop {}
 }
+
 fn clear_bss() {
     unsafe extern "C" {
         safe fn sbss();
