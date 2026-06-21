6e08f79f742655b0a78f6ac6a223085b85af1035
shyweeds <2149934895@qq.com>
2026-06-21 17:25:55 +0800

    racho: add global memory allocator!

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+#![feature(alloc_error_handler)]
 
 #[path = "boards/qemu.rs"]
 mod boards;
@@ -11,15 +12,19 @@ mod config;
 mod lang_items;
 mod loader;
 mod logging;
+mod mm;
 mod sbi;
 mod sync;
 pub mod syscall;
 pub mod task;
 mod timer;
 pub mod trap;
+extern crate alloc;
 
 use core::arch::global_asm;
 use log::*;
+
+use crate::mm::heap_allocator::init_heap;
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
 
@@ -36,16 +41,43 @@ fn clear_bss() {
     }
 }
 
+#[allow(unused)]
+fn heap_test() {
+    use alloc::boxed::Box;
+    use alloc::vec::Vec;
+    unsafe extern "C" {
+        safe fn sbss();
+        safe fn ebss();
+    }
+    let bss_range = sbss as *const () as usize..ebss as *const () as usize;
+    let a = Box::new(5);
+    assert_eq!(*a, 5);
+    assert!(bss_range.contains(&(a.as_ref() as *const _ as *const () as usize)));
+    drop(a);
+    let mut v: Vec<usize> = Vec::new();
+    for i in 0..500 {
+        v.push(i);
+    }
+    for i in 0..500 {
+        assert_eq!(v[i], i);
+    }
+    assert!(bss_range.contains(&(v.as_ptr() as *const _ as *const () as usize)));
+    drop(v);
+    println!("heap_test passed!");
+}
+
 /// the rust entry-point of OS
 #[unsafe(no_mangle)]
 pub fn rust_main() -> ! {
     clear_bss();
     logging::init();
     info!("[kernel] Hello, world!");
-    trap::init();
-    loader::load_apps();
-    trap::enable_timer_interrupt();
-    timer::set_next_trigger();
-    task::run_first_task();
+    init_heap();
+    heap_test();
+    // trap::init();
+    // loader::load_apps();
+    // trap::enable_timer_interrupt();
+    // timer::set_next_trigger();
+    // task::run_first_task();
     panic!("Unreachable in rust_main");
 }
