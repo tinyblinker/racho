022f4349b5fed46ad03e6e54ec75c7f3b9ce6731
shyweeds <2149934895@qq.com>
2026-06-22 22:20:27 +0800

    implying the SV39's wrapper

--- os/src/config.rs
+++ os/src/config.rs
@@ -6,3 +6,5 @@ pub const APP_SIZE_LIMIT: usize = 0x20000;
 pub const USER_STACK_SIZE: usize = 4096 * 2;
 pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
 pub const KERNEL_HEAP_SIZE: usize = 0x30_0000;
+
+pub const PAGE_SIZE_BITS: usize = 0xc;
---
+++ os/src/mm/address.rs
@@ -0,0 +1,17 @@
+use crate::config::PAGE_SIZE_BITS;
+
+/// Physical address
+const PA_WIDTH_SV39: usize = 56;
+const PPN_WIDTH_SV39: usize = PA_WIDTH_SV39 - PAGE_SIZE_BITS;
+
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub struct PhysAddr(pub usize);
+
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub struct VirtAddr(pub usize);
+
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub struct PhysPageNum(pub usize);
+
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub struct VirtPageNum(pub usize);
